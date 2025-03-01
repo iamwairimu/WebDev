6b1f27912ddfb3f50d1024ae0ddbc69f71d8b286
iamwairimu <kmnleah@gmail.com>
2025-03-01 17:33:29 +0200

    second theme

---
+++ highlight.php
@@ -0,0 +1,53 @@
+      <!-- Highlights -->
+      <section id="highlights" class="wrapper style3">
+        <div class="title">The Endorsements</div>
+        <div class="container">
+          <div class="row aln-center">
+            <div class="col-4 col-12-medium">
+              <section class="highlight">
+                <a href="#" class="image featured">
+                <img src="<?php echo get_template_directory_uri(); ?>/img/pic02.jpg" alt="Image">
+               </a>
+                <h3><a href="#">Aliquam diam consequat</a></h3>
+                <p>
+                  Eget mattis at, laoreet vel amet sed velit aliquam diam ante,
+                  dolor aliquet sit amet vulputate mattis amet laoreet lorem.
+                </p>
+                <ul class="actions">
+                  <li><a href="#" class="button style1">Learn More</a></li>
+                </ul>
+              </section>
+            </div>
+            <div class="col-4 col-12-medium">
+              <section class="highlight">
+                <a href="#" class="image featured" >
+                    <img src="<?php echo get_template_directory_uri(); ?>/img/pic03.jpg" alt="Image">
+               </a>
+                <h3><a href="#">Nisl adipiscing sed lorem</a></h3>
+                <p>
+                  Eget mattis at, laoreet vel amet sed velit aliquam diam ante,
+                  dolor aliquet sit amet vulputate mattis amet laoreet lorem.
+                </p>
+                <ul class="actions">
+                  <li><a href="#" class="button style1">Learn More</a></li>
+                </ul>
+              </section>
+            </div>
+            <div class="col-4 col-12-medium">
+              <section class="highlight">
+                <a href="#" class="image featured" >
+                    <img src="<?php echo get_template_directory_uri(); ?>/img/pic04.jpg" alt="Image">
+                </a>
+                <h3><a href="#">Mattis tempus lorem</a></h3>
+                <p>
+                  Eget mattis at, laoreet vel amet sed velit aliquam diam ante,
+                  dolor aliquet sit amet vulputate mattis amet laoreet lorem.
+                </p>
+                <ul class="actions">
+                  <li><a href="#" class="button style1">Learn More</a></li>
+                </ul>
+              </section>
+            </div>
+          </div>
+        </div>
+      </section>
