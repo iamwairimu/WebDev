1b035a72adca9a937ada9ceac4420bf1d2335cae
iamwairimu <kmnleah@gmail.com>
2025-03-10 23:17:14 +0200

    Done with the upper sections

--- front-page.php
+++ front-page.php
@@ -1,4 +1,31 @@
-      <!-- Highlights -->
+<?php get_header(); ?>
+
+<main id="main-content">
+    <div class="container">
+        <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+        <?php $excluded_titles = ["Hello world!", "Welcome to our amazing website redesign!", "My second WordPress Post", "Home", "DropDown", "Left SideBar", "Right SideBar", "No SideBar"];
+        if (in_array(get_the_title(), $excluded_titles)) {
+            continue;
+        } ?>
+        <?php endwhile; else : ?>
+            <p><?php _e('No posts found.') ?></p>
+        <?php endif; ?>
+    </div>
+</main>
+
+  <!-- Wordpress loop -->
+
+                <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+     
+                <?php the_content(); ?>
+
+                <?php endwhile; else: ?>
+                    <p><?php _e('Sorry, no posts matched your criteria') ?></p>
+                <?php endif; ?>
+
+            <!-- End WordPress loop -->
+          
+<!-- Highlights -->
       <section id="highlights" class="wrapper style3">
         <div class="title">The Endorsements</div>
         <div class="container">
@@ -51,3 +78,5 @@
           </div>
         </div>
       </section>
+      
+<?php get_footer(); ?>
--- header.php
+++ header.php
@@ -5,7 +5,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Escape Velocity by HTML5 UP</title>
     <link rel="stylesheet" href="<?php bloginfo("stylesheet_url"); ?>"/>
-
     </head>
 
 <body <?php body_class(); ?>>
